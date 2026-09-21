54d92ff622c565797314c87243d6956c07a9579d
Ludimila-Araujo <dearaujo.ludimila@gmail.com>
2026-09-20 21:05:56 -0300

    feat: add species overview processing

--- src/hfpef_catalog/processing.py
+++ src/hfpef_catalog/processing.py
@@ -11,3 +11,29 @@ def sum_all_repositories(df_species):
         total_repo += species_repositories
 
     return total_repo
+
+
+# Função para definir métricas iniciais das espécies:
+def get_species_overview(df):
+
+    overview_columns = df[
+        [
+            "Dataset_ID",
+            "Sampled_Tissue",
+            "Strain / Ethnicity",
+            "Sex",
+            "N_BioSamples",
+            "Condition: {n}",
+        ]
+    ]
+
+    overview_dict = overview_columns.loc[0].to_dict()
+
+    return {
+        "Dataset ID": overview_dict["Dataset_ID"],
+        "Sampled tissue": overview_dict["Sampled_Tissue"],
+        "Strain / ethnicity": overview_dict["Strain / Ethnicity"],
+        "Sex": overview_dict["Sex"],
+        "Biosamples": overview_dict["N_BioSamples"],
+        "Conditions": overview_dict["Condition: {n}"],
+    }
